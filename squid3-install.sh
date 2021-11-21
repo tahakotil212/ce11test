6cb49eaf26ccdf676e4676d495a708970993a190
tahakotil212 <94573457+tahakotil212@users.noreply.github.com>
2021-11-21 06:40:34 +0300

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3
@@ -17,51 +16,8 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04"; then
     /sbin/iptables -I INPUT -p tcp --dport 3135 -j ACCEPT
     service squid restart
     systemctl enable squid
-
-elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
-    # OS = Debian 8
-    /bin/rm -rf /etc/squid
-    /usr/bin/apt update
-    /usr/bin/apt -y install apache2-utils squid3
-    touch /etc/squid3/passwd
-    /bin/rm -f /etc/squid3/squid.conf
-    /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/londonertech/createproxy/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-    /sbin/iptables-save
-    service squid3 restart
-    update-rc.d squid3 defaults
-    ln -s /etc/squid3 /etc/squid
-elif cat /etc/os-release | grep PRETTY_NAME | grep "stretch"; then
-    # OS = Debian 9
-    /bin/rm -rf /etc/squid
-    /usr/bin/apt update
-    /usr/bin/apt -y install apache2-utils squid
-    touch /etc/squid/passwd
-    /bin/rm -f /etc/squid/squid.conf
-    /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/londonertech/createproxy/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-    /sbin/iptables-save
-    systemctl enable squid
-    systemctl restart squid
-elif cat /etc/os-release | grep PRETTY_NAME | grep "buster"; then
-    # OS = Debian 10
-    /bin/rm -rf /etc/squid
-    /usr/bin/apt update
-    /usr/bin/apt -y install apache2-utils squid
-    touch /etc/squid/passwd
-    /bin/rm -f /etc/squid/squid.conf
-    /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/londonertech/createproxy/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-    /sbin/iptables-save
-    systemctl enable squid
-    systemctl restart squid
 else
-    echo "OS NOT SUPPORTED.\n"
+    echo "Ubuntu 18.04 Destekler."
     echo "Contact admin@serverok.in to add support for your os."
     exit 1;
-fi
-
-#/usr/bin/htpasswd -b -c /etc/squid/passwd USERNAME_HERE PASSWORD_HERE
+    fi
